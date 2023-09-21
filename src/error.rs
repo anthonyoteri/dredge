cead0006a488067c305c6aea24c12fc100958e30
Anthony Oteri <anthony.oteri@edgeware.tv>
2023-09-21 15:56:34 -0400

    Miscellaneous code cleanup

--- src/error.rs
+++ src/error.rs
@@ -21,10 +21,6 @@ use thiserror::Error;
 /// The common error type for this Application.
 #[derive(Error, Debug)]
 pub enum DredgeError {
-    /// An error related to the configuration of the program.
-    #[error(transparent)]
-    ConfigError(#[from] ConfigError),
-
     /// An error communicating with the Registry API
     #[error(transparent)]
     ApiError(#[from] ApiError),
@@ -32,40 +28,6 @@ pub enum DredgeError {
     /// An error building the registry URL
     #[error("Error determining registry URL from {0}")]
     RegistryUrlError(String),
-}
-
-/// An error related to the configuration fo the program.
-#[derive(Error, Debug)]
-pub enum ConfigError {
-    /// An error parsing the configuration from disk.
-    #[error("Failed to parse configuration file")]
-    ParseError(Box<dyn std::error::Error>),
-
-    /// An error writing the configuration to disk.
-    #[error("Failed to write configuration data")]
-    WriteError(Box<dyn std::error::Error>),
-
-    /// A generic IOError
-    #[error(transparent)]
-    IOError(#[from] std::io::Error),
-}
-
-impl From<toml::ser::Error> for ConfigError {
-    fn from(other: toml::ser::Error) -> Self {
-        Self::WriteError(Box::from(other))
-    }
-}
-
-impl From<toml::de::Error> for ConfigError {
-    fn from(other: toml::de::Error) -> Self {
-        Self::ParseError(Box::from(other))
-    }
-}
-
-impl From<xdg::BaseDirectoriesError> for ConfigError {
-    fn from(other: xdg::BaseDirectoriesError) -> Self {
-        Self::WriteError(Box::from(other))
-    }
 }
 
 /// An error related to the communication with the registry API.
